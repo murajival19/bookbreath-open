7d7d5f22cb3cf354afd616261906ffe48bc3cf28
Jun Kawamura <progprojk929@gmail.com>
2020-06-03 17:16:50 +0900

    Laravelのバージョンを7に変更

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@
 //     return view('welcome');
 // });
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
