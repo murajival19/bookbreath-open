8401db6f0cbbe8f9f9729688666a9e6c609096f0
jun-kawamura <progprojk929@gmail.com>
2020-06-10 17:51:43 +0900

    BookLikeControllerのリファクタリング

--- app/Http/Controllers/BooklikeController.php
+++ app/Http/Controllers/BooklikeController.php
@@ -3,27 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Booklike;
+use App\Repository\BookLikeRepository;
 
+/**
+ * 本のいいねに関するコントローラクラス
+ */
 class BooklikeController extends Controller
 {
+    /**
+     * 指定した本にいいねをします。
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
     public function like(Request $request)
     {
-        $booklike = new Booklike;
-        $booklike->user_id = $request->user_id;
-        $booklike->book_id = $request->book_id;
-        $booklike->save();
-
-        $likeCount = count(Booklike::where('book_id', $request->book_id)->get());
+        $bookLikeRepository = new BookLikeRepository();
+        $bookLikeRepository->saveBookLike([
+            'user_id' => $request->user_id,
+            'book_id' => $request->book_id,
+        ]);
+        $likeCount = count($bookLikeRepository->getBookLike($request->book_id)->get());
         return response()->json(['likeCount' => $likeCount]);
     }
 
-    public function unlike(Booklike $booklike, Request $request)
+    /**
+     * 指定した本のいいねを削除します。
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function unlike(Request $request)
     {
-        $delete_booklike = $booklike->where('user_id', $request->user_id)->where('book_id', $request->book_id)->first();
-        $delete_booklike->delete();
-
-        $likeCount = count(Booklike::where('book_id', $request->book_id)->get());
+        $bookLikeRepository = new BookLikeRepository();
+        $bookLikeRepository->deleteBookLike($request->user_id, $request->book_id);
+        $likeCount = count($bookLikeRepository->getBookLike($request->book_id)->get());
         return response()->json(['likeCount' => $likeCount]);
     }
 }
